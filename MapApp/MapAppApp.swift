77d2246e3df2b2b81480ddc572ef6a7d4a88155a
Leonardo Almeida <leonardograselalmeida@gmail.com>
2023-02-20 20:28:58 -0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Leonardo Almeida on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
